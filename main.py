4448ba2e83f83f0c2199f0bd48b40db590f4acd9
ericgoodwin <edgbpc@mail.umsl.edu>
2018-12-03 15:07:42 -0600

    changed to let cozmo select the game to run

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import random
 
 
 '''
-color_list order is in order of colorwheel going clockwise
+color_list order is in order of color wheel going clockwise
 '''
 color_list = ["yellow", "green", "blue", "purple", "red", "orange"]
 primary_color_list = ['yellow', 'blue', 'red']
@@ -23,9 +23,10 @@ game_selector = 0 - Cozmo provides two primary colors and user displays 1 second
 game_selector = 1 - Cozmo provides one primary or secondary color and user days complimentary color according to color
 wheel
 '''
-game_selector = 1
+# game_selector = 1
 
-# game_selector = random.randint(0, 1)
+# determine which game is going to run
+game_selector = random.randint(0, 1)
 
 
 
@@ -48,12 +49,17 @@ async def gameOne_cozmo_program(robot: cozmo.robot.Robot):
 
 
 
+
 async def gameTwo_cozmo_program(robot: cozmo.robot.Robot):
+
+    # colors_not_different purpose to ensure that Cozmo doesn't select te same primary color for both inputs
     colors_not_different = True
     color_to_pass = ""
 
+    # select a random primary color
     primary_color_1_selected_index = random.randint(0, 2)
 
+    # select a second random primary color.  the while loop ensures the same color is not selected twice
     while colors_not_different:
         primary_color_2_selected_index = random.randint(0, 2)
         if primary_color_1_selected_index is not primary_color_2_selected_index:
@@ -62,12 +68,28 @@ async def gameTwo_cozmo_program(robot: cozmo.robot.Robot):
     # print(primary_color_1_selected_index)
     # print(primary_color_2_selected_index)
 
+
+    # selects the color that cozmo will look for
+
+    # this formula determines what color is selected that cozmo is looking for as for the chart below
+    # if correct_color_to_find = 1 : Green
+    # if correct_color_to_find = 2 : Orange
+    # if correct_color_to_find = 3 : Purple
+
     correct_color_to_find = primary_color_1_selected_index + primary_color_2_selected_index
+
+
+    # cozmo will speak the two colors that he is looking forward
     await robot.say_text("What color is made by " + primary_color_list[primary_color_1_selected_index] +
                          " and" + primary_color_list[primary_color_2_selected_index]).wait_for_completed()
 
+
+    # declare the object color)_finder_game
+    # ARGS - robot, and array color_list and index correct_color_to_find
     color_finder_game = color_finder.ColorFinder(robot, color_list[correct_color_to_find])
 
+
+    # run the color_finder_object
     await color_finder_game.run()
 
 
@@ -79,27 +101,10 @@ async def gameTwo_cozmo_program(robot: cozmo.robot.Robot):
         color_to_pass = "purple"
 
 
-    print(color_to_pass)
-    print(correct_color_to_find)
+    # print(color_to_pass)
+    # print(correct_color_to_find)
 
-
-
-
-
-
-
-
-
-
-
-
-# Cozmo waits and looks for 1 of 2 colors
-
-
-
-    # await colorFinder2.run()
-
-   # await robot.say_text("I found yellow").wait_for_completed()
+    await robot.say_text(color_to_pass + "is correct!  Great job!").wait_for_completed()
 
 
 # modify color_finder to search for specific colors
@@ -115,3 +120,5 @@ async def gameTwo_cozmo_program(robot: cozmo.robot.Robot):
 
 if game_selector == 1:
     cozmo.run_program(gameTwo_cozmo_program, use_viewer=True)
+else:
+    cozmo.run_program(gameOne_cozmo_program, use_viewer=True)
